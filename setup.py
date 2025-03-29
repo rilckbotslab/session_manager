55444482913150e4ad719e29033ece5e9ac8ef05
rilckbotslab <rilck.lima@botslab.com.br>
2025-03-29 15:13:39 -0300

    Rename package to session_manager and update setup.py with description and classifiers

--- setup.py
+++ setup.py
@@ -11,19 +11,21 @@ if os.path.exists("README.md"):
     README = open("README.md").read()
 
 setup(
-    name="botslab",
+    name="session_manager",
     version="0.1.0",
+    description="A session manager for bot automation",
     long_description=README,
     long_description_content_type="text/markdown",
     author="rilckbotslab",
     author_email="rilck.lima@botslab.com.br",
     url="https://github.com/rilckbotslab/session_manager",
-    install_requires=REQUIRED_PACKAGES,    
-    packages=find_packages(
-        include=[
-            "session_manager",
-            "session_manager.*",
-        ]
-    ),
+    install_requires=REQUIRED_PACKAGES,
+    packages=find_packages(exclude=["tests", "tests.*"]),
+    python_requires=">=3.7",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
     platforms="any",
 )
